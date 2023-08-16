0f50b5729b76d01c3e55411f042ebb70d27e5cd5
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-08-16 18:06:53 -0300

    Salvando e listando cidades

--- src/main/java/io/github/WesleyViricimo/LocalizacaoApplication.java
+++ src/main/java/io/github/WesleyViricimo/LocalizacaoApplication.java
@@ -1,15 +1,34 @@
 package io.github.WesleyViricimo;
 
+import io.github.WesleyViricimo.domain.entity.Cidade;
+import io.github.WesleyViricimo.domain.repository.CidadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.transaction.Transactional;
+
 @SpringBootApplication
 public class LocalizacaoApplication implements CommandLineRunner {
 
+    @Autowired
+    private CidadeRepository repository;
+
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Inicializando");
+        salvarCidade();
+        listarCidade();
+    }
+
+    @Transactional
+    void salvarCidade(){
+        var cidade = new Cidade(1, "São Paulo", 6000000L);
+        repository.save(cidade);
+    }
+
+    void listarCidade(){
+        repository.findAll().forEach(System.out::println);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/io/github/WesleyViricimo/domain/repository/CidadeRepository.java
@@ -0,0 +1,8 @@
+package io.github.WesleyViricimo.domain.repository;
+
+import io.github.WesleyViricimo.domain.entity.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
